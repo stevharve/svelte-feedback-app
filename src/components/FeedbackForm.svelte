c8ba91e43b315d393a988c406af02c19e24c842d
Liam Brewer <liam.a.brewer@gmail.com>
2022-03-05 11:52:31 -0600

    Initial commit

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<form>
+  <!-- Rating Select-->
+  <div class="input-group">
+    <input type="text" placeholder="Tell us something that keeps you coming back">
+    <button>Send</button>
+  </div>
+</form>
+
+<style>
+  
+</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import FeedbackItem from "./FeedbackItem.svelte"
+  export let feedback: any[] = []
+
+  console.log(feedback)
+</script>
+
+{#each feedback as fb (fb.id)}
+  <FeedbackItem item={fb} on:delete-feedback />
+{/each}
